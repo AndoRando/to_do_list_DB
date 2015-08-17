c2fbcbf80714bfc8def33f10aaa452e2089b4d26
Andrew Brennwald & Jennifer Bell <student@epicodus.com>
2015-08-17 13:58:14 -0700

    Implement all tasks method for the list class.

--- lib/task.rb
+++ lib/task.rb
@@ -1,12 +1,12 @@
 class Task
   attr_reader(:description, :list_id)
 
-  define_method (:initialize) do |attributes|
+  define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
   end
 
-  define_singleton_method (:all) do
+  define_singleton_method(:all) do
     returned_tasks = DB.exec("SELECT * FROM tasks;")
     tasks = []
     returned_tasks.each() do |task|
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -9,32 +9,54 @@ describe(List) do
 
   describe("#name") do
     it("tells you its name") do
-      list = List.new({:name => "Epicodus stuff", :id => nil})
-      expect(list.name()).to(eq("Epicodus stuff"))
+      test_list = List.new({:name => "Epicodus stuff", :id => nil})
+      expect(test_list.name()).to(eq("Epicodus stuff"))
+    end
+  end
+
+  describe("#tasks") do
+    it("returns an array of tasks for that list") do
+      test_list = List.new({:name => "Epicodus stuff", :id => nil})
+      test_list.save()
+      test_task = Task.new({:description => "Learn SQL", :list_id => test_list.id()})
+      test_task.save()
+      test_task2 = Task.new({:description => "Review Ruby", :list_id => test_list.id()})
+      test_task2.save()
+      expect(test_list.tasks()).to(eq([test_task, test_task2]))
     end
   end
 
   describe("#id") do
     it("sets its ID when you save it") do
-      list = List.new({:name => "Epicodus stuff", :id => nil})
-      list.save()
-      expect(list.id()).to(be_an_instance_of(Fixnum))
+      test_list = List.new({:name => "Epicodus stuff", :id => nil})
+      test_list.save()
+      expect(test_list.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe(".find") do
+    it("returns a list by its ID") do
+      test_list = List.new({:name => "Epicodus stuff", :id => nil})
+      test_list.save()
+      test_list2 = List.new({:name => "Home stuff", :id => nil})
+      test_list2.save()
+      expect(List.find(test_list2.id())).to(eq(test_list2))
     end
   end
 
   describe("#save") do
     it("lets you save lists to the database") do
-      list = List.new({:name => "Epicodus stuff", :id => nil})
-      list.save()
-      expect(List.all()).to(eq([list]))
+      test_list = List.new({:name => "Epicodus stuff", :id => nil})
+      test_list.save()
+      expect(List.all()).to(eq([test_list]))
     end
   end
 
   describe("#==") do
     it("is the same list if it has the same name") do
-      list1 = List.new({:name => "Epicodus stuff", :id => nil})
-      list2 = List.new({:name => "Epicodus stuff", :id => nil})
-      expect(list1).to(eq(list2))
+      test_list = List.new({:name => "Epicodus stuff", :id => nil})
+      test_list2 = List.new({:name => "Epicodus stuff", :id => nil})
+      expect(test_list).to(eq(test_list2))
     end
   end
 end
